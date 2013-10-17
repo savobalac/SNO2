a7ecfcecaab31ecf5cf02d9bc18d21414257097a
Savo <savobalac@yahoo.co.uk>
2013-10-17 06:38:47 +0100

    Added Build.scala to Git.

---
+++ project/Build.scala
@@ -0,0 +1,22 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "SNO2"
+    val appVersion      = "1.0"
+
+    val appDependencies = Seq(
+        // Add your project dependencies here,
+        "mysql" % "mysql-connector-java" % "5.1.21",
+        "org.reflections" % "reflections" % "0.9.8",
+        javaCore, javaJdbc, javaEbean
+    )
+
+    val main = play.Project(appName, appVersion, appDependencies).settings(
+      // Add your own project settings here
+      testOptions in Test += Tests.Argument("junitxml", "console")
+    )
+
+}
