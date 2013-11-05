9830d1c432d9b6e9eb21680fe614e338642b9c2e
Savo <savobalac@yahoo.co.uk>
2013-11-05 14:26:00 +0000

    Added some test cases that rely on a fake application and model classes. Nowhere near complete.

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import models.Users;
 import org.junit.*;
 
+import play.libs.WS;
 import play.mvc.*;
 import play.test.*;
 import play.data.DynamicForm;
@@ -16,6 +17,7 @@ import play.i18n.Lang;
 import play.libs.F;
 import play.libs.F.*;
 
+import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 
@@ -34,26 +36,72 @@ public class ApplicationTest {
         assertThat(a).isEqualTo(2);
     }
 
+    @Before
+    public void setUp() {
+        //start(fakeApplication(inMemoryDatabase()));
+        start(fakeApplication());
+    }
+
     @Test
     public void findById() {
-        running(fakeApplication(), new Runnable() {
-            public void run() {
+        //running(fakeApplication(), new Runnable() {
+            //public void run() {
                 /*Computer macintosh = Computer.find.byId(21l);
                 assertThat(macintosh.name).isEqualTo("Macintosh");
                 assertThat(formatted(macintosh.introduced)).isEqualTo("1984-01-24");*/
-            }
-        });
+                Users user = Users.find.byId(3L); // User 3 has username savbalac
+                if (user != null) {
+                    assertThat(user.username).isEqualTo("savbalac");
+                    Content html = views.html.index.render("SNO2", user);
+                    assertThat(contentType(html)).isEqualTo("text/html");
+                    assertThat(contentAsString(html)).contains("savbalac");
+                } else {
+                    System.out.println("User, id: 3 is null");
+                }
+            //}
+        //});
     }
 
-    /*@Test
+    @Test
     public void renderTemplate() {
         //Content html = views.html.index.render("Your new application is ready.");
-        Users user = Users.find.byId(3L); // User 3 has username savbalac
+        /*Users user = Users.find.byId(3L); // User 3 has username savbalac
         Content html = views.html.index.render("SNO2", user);
         assertThat(contentType(html)).isEqualTo("text/html");
         //assertThat(contentAsString(html)).contains("Your new application is ready.");
-        assertThat(contentAsString(html)).contains("savbalac");
-    }*/
+        assertThat(contentAsString(html)).contains("savbalac");*/
+    }
 
+    @Test
+    // Test the application controller's index method
+    public void callIndex() {
+        Users user = Users.find.byId(3L); // User 3 has username savbalac
+        Result result = callAction(
+                controllers.routes.ref.Application.index()
+        );
+        assertThat(status(result)).isEqualTo(OK);
+        assertThat(contentType(result)).isEqualTo("text/html");
+        assertThat(charset(result)).isEqualTo("utf-8");
+        assertThat(contentAsString(result)).contains("savbalac");
+    }
+
+    @Test
+    // Test the router
+    public void badRoute() {
+        Result result = routeAndCall(fakeRequest(GET, "/xx/Kiki"));
+        assertThat(result).isNull();
+    }
+
+    @Test
+    // Start a test server
+    public void testInServer() {
+        running(testServer(3333), new Runnable() {
+            public void run() {
+                assertThat(
+                        WS.url("http://localhost:3333").get().get().getStatus()
+                ).isEqualTo(OK);
+            }
+        });
+    }
 
 }
--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -15,9 +15,26 @@ public class IntegrationTest {
      * add your integration test here
      * in this example we just check if the welcome page is being shown
      */
-    @Test
+    /*@Test
     public void test() {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+            public void invoke(TestBrowser browser) {
+                browser.goTo("http://localhost:3333");
+                assertThat(browser.pageSource()).contains("Sign in to SNO2");
+            }
+        });
+    }*/
+
+    @Before
+    public void setUp() {
+        //start(fakeApplication(inMemoryDatabase()));
+        start(fakeApplication());
+    }
+
+    @Test
+    // Run a fake application
+    public void test() {
+        running(testServer(3333, fakeApplication()), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
                 browser.goTo("http://localhost:3333");
                 assertThat(browser.pageSource()).contains("Sign in to SNO2");
