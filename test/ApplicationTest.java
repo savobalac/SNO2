a97d4339cfb2768b42d4e3b346613282f1425afb
Savo <savobalac@yahoo.co.uk>
2013-11-06 07:37:24 +0000

    Basic Play! test methods added (and login page uses "Sign in" not "Log in").

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import controllers.Application;
 import models.Users;
+import org.fluentlenium.core.domain.FluentWebElement;
 import org.junit.*;
 
 import play.libs.WS;
@@ -19,36 +21,64 @@ import play.libs.F.*;
 
 import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.*;
+import static play.data.Form.*;
 import static org.fest.assertions.Assertions.*;
 
-
 /**
-*
-* Simple (JUnit) tests that can call all parts of a play app.
-* If you are interested in mocking a whole application, see the wiki for more details.
-*
-*/
+ * Created with IntelliJ IDEA.
+ * User: Sav Balac
+ * Date: 06/11/13
+ * Time: 05:47
+ * Description: Application test that tests the main parts of the application.
+ * To change this template use File | Settings | File Templates.
+ */
 public class ApplicationTest {
 
-    @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
-    }
 
+    /**
+     * @verifies That the application has started.
+     */
     @Before
     public void setUp() {
-        //start(fakeApplication(inMemoryDatabase()));
         start(fakeApplication());
     }
 
+
+    /**
+     * @verifies That the login page is the first page displayed.
+     */
+    @Test
+    public void testLoginPage() {
+        running(testServer(3333, fakeApplication()), HTMLUNIT, new Callback<TestBrowser>() {
+            public void invoke(TestBrowser browser) {
+                browser.goTo("http://localhost:3333");
+                assertThat(browser.pageSource()).contains("Sign in to SNO2");
+            }
+        });
+    }
+
+
+    /**
+     * @verifies That the logged-in user sees the index page.
+     */
+    @Test
+    public void testIndexTemplate() {
+        //Users user = Users.find.byId(1L); // User id 1 has username savbalac
+        // The next line gives error: java.lang.RuntimeException: There is no HTTP Context available from here.
+        //Content html = views.html.index.render("SNO2", user);
+        //assertThat(contentType(html)).isEqualTo("text/html");
+        //assertThat(contentAsString(html)).contains("Donec id elit non mi porta gravida at eget metus.");
+    }
+
+
+    /*
     @Test
     public void findById() {
         //running(fakeApplication(), new Runnable() {
             //public void run() {
-                /*Computer macintosh = Computer.find.byId(21l);
-                assertThat(macintosh.name).isEqualTo("Macintosh");
-                assertThat(formatted(macintosh.introduced)).isEqualTo("1984-01-24");*/
+                //Computer macintosh = Computer.find.byId(21l);
+                //assertThat(macintosh.name).isEqualTo("Macintosh");
+                //assertThat(formatted(macintosh.introduced)).isEqualTo("1984-01-24");
                 Users user = Users.find.byId(3L); // User 3 has username savbalac
                 if (user != null) {
                     assertThat(user.username).isEqualTo("savbalac");
@@ -60,48 +90,74 @@ public class ApplicationTest {
                 }
             //}
         //});
-    }
+    }*/
 
-    @Test
-    public void renderTemplate() {
-        //Content html = views.html.index.render("Your new application is ready.");
-        /*Users user = Users.find.byId(3L); // User 3 has username savbalac
-        Content html = views.html.index.render("SNO2", user);
-        assertThat(contentType(html)).isEqualTo("text/html");
-        //assertThat(contentAsString(html)).contains("Your new application is ready.");
-        assertThat(contentAsString(html)).contains("savbalac");*/
-    }
 
-    @Test
-    // Test the application controller's index method
-    public void callIndex() {
-        Users user = Users.find.byId(3L); // User 3 has username savbalac
-        Result result = callAction(
-                controllers.routes.ref.Application.index()
-        );
-        assertThat(status(result)).isEqualTo(OK);
-        assertThat(contentType(result)).isEqualTo("text/html");
-        assertThat(charset(result)).isEqualTo("utf-8");
-        assertThat(contentAsString(result)).contains("savbalac");
-    }
 
+    /**
+     * @verifies That the logged-in user sees the home page.
+     */
     @Test
-    // Test the router
-    public void badRoute() {
-        Result result = routeAndCall(fakeRequest(GET, "/xx/Kiki"));
-        assertThat(result).isNull();
-    }
-
-    @Test
-    // Start a test server
-    public void testInServer() {
-        running(testServer(3333), new Runnable() {
+    public void testIndex() {
+        running(fakeApplication(), new Runnable() {
             public void run() {
-                assertThat(
-                        WS.url("http://localhost:3333").get().get().getStatus()
-                ).isEqualTo(OK);
+                Users user = Users.find.byId(1L); // User 1 has username savbalac
+                Result res = route(fakeRequest("GET", "/")
+                        .withSession("message", "SNO2")
+                        .withSession("user", "savbalac"));
+                assert(contentAsString(res).contains("Donec id elit non mi porta gravida at eget metus."));
+                assert(contentAsString(res).contains("savbalac"));
             }
         });
     }
 
+
+    /**
+     * @verifies That an incorrect route gives null.
+     */
+    @Test
+    public void testBadRoute() {
+        Result result = route(fakeRequest(GET, "/xx/Kiki"));
+        assertThat(result).isNull();
+    }
+
+
+
+    /**
+     * @verifies That the real HTTP stack is running and that the login page is displayed.
+     */
+    @Test
+    public void testInServer() {
+        running(testServer(3333), new Runnable() {
+            public void run() {
+                assertThat(WS.url("http://localhost:3333").get().get(2000).getStatus()).isEqualTo(OK); // 2000ms timeout
+                assertThat(WS.url("http://localhost:3333").get().get(2000).getBody().contains("Sign in to SNO2"));
+            }
+        });
+    }
+
+    @Test
+    public void runInBrowser() {
+        running(testServer(3333), HTMLUNIT, new Callback<TestBrowser>() {
+            public void invoke(TestBrowser browser) {
+                browser.goTo("http://localhost:3333");
+                //assertThat(browser.$("#title").getTexts().get(0)).isEqualTo("Sign in to SNO2"); // IndexOutOfBoundsException
+                // Next line gives: org.openqa.selenium.NoSuchElementException:
+                //     No such element with position :0. Number of elements available :0
+                //assertThat(browser.$("#title", 0).getText()).isEqualTo("Sign in to SNO2");
+                browser.$("submit").click();
+                assertThat(browser.url()).isEqualTo("http://localhost:3333/login");
+                //assertThat(browser.$("#title", 0).getText()).isEqualTo("SNO2");
+            }
+        });
+    }
+
+
+    /**
+     * @verifies That the application has stopped.
+     */
+    @After
+    public void tearDown() {
+        stop(fakeApplication());
+    }
 }
--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,39 +1,35 @@
 import org.junit.*;
-
-import play.mvc.*;
 import play.test.*;
 import play.libs.F.*;
-
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 
-import static org.fluentlenium.core.filter.FilterConstructor.*;
 
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sav Balac
+ * Date: 06/11/13
+ * Time: 05:47
+ * Description: Integration test that just starts the application and checks the sign in page is showing.
+ * To change this template use File | Settings | File Templates.
+ */
 public class IntegrationTest {
 
-    /**
-     * add your integration test here
-     * in this example we just check if the welcome page is being shown
-     */
-    /*@Test
-    public void test() {
-        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
-            public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333");
-                assertThat(browser.pageSource()).contains("Sign in to SNO2");
-            }
-        });
-    }*/
 
+    /**
+     * @verifies That the application has started.
+     */
     @Before
     public void setUp() {
-        //start(fakeApplication(inMemoryDatabase()));
         start(fakeApplication());
     }
 
+
+    /**
+     * @verifies That the sign in page is current.
+     */
     @Test
-    // Run a fake application
-    public void test() {
+    public void testSignIn() {
         running(testServer(3333, fakeApplication()), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
                 browser.goTo("http://localhost:3333");
@@ -41,5 +37,15 @@ public class IntegrationTest {
             }
         });
     }
+
+
+    /**
+     * @verifies That the application has stopped.
+     */
+    @After
+    public void tearDown() {
+        stop(fakeApplication());
+    }
+
 
 }
