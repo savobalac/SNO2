5bbe51af493f680524393b7c09ee20a6de708cef
Savo <savobalac@yahoo.co.uk>
2013-11-11 11:11:41 +0000

    Remove the Groups controller and views (groups are a static list).

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -29,8 +29,7 @@ public class Application extends Controller {
 
     // Add a constant here when creating a new list page
     public static final int PAGE_TYPE_ANALYSTS  =  1;
-    public static final int PAGE_TYPE_GROUPS    =  2;
-    public static final int PAGE_TYPE_USERS     =  3;
+    public static final int PAGE_TYPE_USERS     =  2;
 
 
     /**
@@ -97,8 +96,6 @@ public class Application extends Controller {
         switch (pageType) {
             case PAGE_TYPE_ANALYSTS:
                 return controllers.routes.Analysts.list(page, sortBy, sortOrder, filter1, filter2);
-            case PAGE_TYPE_GROUPS:
-                return controllers.routes.Groups.list(page, sortBy, sortOrder, filter1, filter2);
             case PAGE_TYPE_USERS:
                 return controllers.routes.Users.list(page, sortBy, sortOrder, filter1, filter2);
             default:
