b1118a88c68b6eaa01e25b9deb03170374c67c6f
Savo <savobalac@yahoo.co.uk>
2013-11-27 13:25:21 +0000

    Configured the JDBC pool using BoneCP 0.8.0 to prevent the DB from timing out and causing SQLExceptions.

--- project/Build.scala
+++ project/Build.scala
@@ -12,12 +12,14 @@ object ApplicationBuild extends Build {
         "mysql" % "mysql-connector-java" % "5.1.21",
         "org.reflections" % "reflections" % "0.9.8",
         javaCore, javaJdbc, javaEbean,
-        "com.amazonaws" % "aws-java-sdk" % "1.3.11"
+        "com.amazonaws" % "aws-java-sdk" % "1.3.11",
+        "com.jolbox" % "bonecp" % "0.8.0-rc2-SNAPSHOT-20130712-14382677.jar"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here
-      testOptions in Test += Tests.Argument("junitxml", "console")
+      testOptions in Test += Tests.Argument("junitxml", "console"),
+      libraryDependencies += "com.jolbox" % "bonecp" % "0.8.0-rc2-SNAPSHOT-20130712-14382677.jar" from "https://dl.dropboxusercontent.com/u/36714110/libraries/bonecp-patches/bonecp-0.8.0-rc2-SNAPSHOT-20130712-14382677.jar"
     )
 
 }
